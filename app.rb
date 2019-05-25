a7606c6ae4a6f60e3c65f19d012180ebdf1939cd
Anchieta Júnior <santosjr87@gmail.com>
2019-05-25 19:41:57 -0300

    Add dependencies, mongoid and views

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require 'sinatra/contrib'
+require 'sinatra/reloader'
+require 'sinatra/namespace'
+require 'mongoid'
+
+Mongoid.load! 'mongoid.config'
+
+class Url
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :original_url, type: String
+  field :shortened_url, type: String
+end
 
 get '/' do
-  "This is an api with Sinatra"
+  erb :index
 end
+
+post '/urls' do
+  headers('Content-Type' => :json)
+  url = Url.new(JSON.parse(request.body.read)).save
+  url.to_json
+end
+
+get ('/version') { "1.0" }
